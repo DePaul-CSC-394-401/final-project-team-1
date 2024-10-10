70c841366b800c6320345902202c9a6ce9e6c536
Diego Izaguirre <tenochca.di@outlook.com>
2024-10-10 18:42:27 -0500

    added explore page layout and simple styling

--- depaul_market/views.py
+++ depaul_market/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def listings(request):
+    return render(request, 'explore.html')
