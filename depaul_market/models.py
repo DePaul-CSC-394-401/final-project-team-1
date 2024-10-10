acf7677a66ab25b5dc1e25a1cd0a45ae67607f5e
Joseph Castillo <jtcastillojr@gmail.com>
2024-10-10 18:46:52 -0500

    Product Model

--- depaul_market/models.py
+++ depaul_market/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Products(models.Model):
+    image = models.ImageField(null=False, blank=False)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    made_avaliable = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['made_avaliable']
