3bbd717a6b1ed1ab10e54dbc13b5bf6de35aaf4e
mansurisaad555 <mansurisaad555@gmail.com>
2024-10-11 01:48:55 -0500

    Created models for login and signup page

--- depaul_market/models.py
+++ depaul_market/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Profile model to store additional student info
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    year = models.CharField(max_length=20, choices=[
+        ('freshman', 'Freshman'),
+        ('sophomore', 'Sophomore'),
+        ('junior', 'Junior'),
+        ('senior', 'Senior'),
+        ('graduate', 'Graduate')
+    ])
+    campus = models.CharField(max_length=50, choices=[
+        ('lincoln_park', 'Lincoln Park'),
+        ('loop', 'Loop')
+    ])
+    graduating = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
