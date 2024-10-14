326ccdf1a7c16b83d0524b14a520f9a728643bbb
ACE17075 <moosashahid6@gmail.com>
2024-10-13 19:53:03 -0500

    Added profile management with email and password update functionality

--- depaul_market/forms.py
+++ depaul_market/forms.py
@@ -1,3 +1,4 @@
+'''
 from django import forms
 from django.forms import ModelForm
 from .models import Products
@@ -9,3 +10,32 @@ class ProductsForm(forms.ModelForm):
     class Meta:
         model = Products
         fields = ['image', 'name', 'price', 'description']
+'''
+
+from django import forms
+from django.forms import ModelForm
+from .models import Products
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+
+# Existing ProductsForm
+class ProductsForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Give Item a Name'}))
+
+    class Meta:
+        model = Products
+        fields = ['image', 'name', 'price', 'description']
+
+
+# New form for updating email
+class EmailUpdateForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Update your email'}))
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+
+# PasswordChangeForm is already provided by Django
+# We don't need to redefine it but we can use it in views like this:
+# password_form = PasswordChangeForm(user=request.user)
--- depaul_market/urls.py
+++ depaul_market/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('explore/', views.listings, name='explore'),
     path('signup', views.signup, name='signup'),
     path('userlogin', views.userlogin, name='userlogin'),
-    path('add_listing', views.addProduct, name='add_listing')
+    path('add_listing', views.addProduct, name='add_listing'),
+    path('profile/', views.profile_settings, name='profile_settings'),
+
 ]
