21542a1401d2b51a7f878ee5aafa5c26b0736044
mansurisaad555 <mansurisaad555@gmail.com>
2024-10-28 18:51:28 -0500

    added urls and redirect to landing page

--- depaul_market/views.py
+++ depaul_market/views.py
@@ -99,7 +99,7 @@ def userlogin(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('explore')
+            return redirect('landing')
         else:
             messages.error(request, "Invalid login credentials.")
     return render(request, 'login.html')
